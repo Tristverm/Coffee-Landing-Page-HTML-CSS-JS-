f952cc8936c5ee3a690607ca9b42c948ed9ab2f6
Tristverm <emmanuelkyalo72@gmail.com>
2024-10-26 12:21:22 +0300

    Added a coffee container styleing, content section, and img-2 styling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
-  const section = document.getElementsByClassName("section-content")[0];
-  if (section) {
-    section.scrollIntoView({ behavior: "smooth" });
-  }
+  const section = document.getElementsByClassName("coffee-container")[0];
+
+  section?.scrollIntoView({ behavior: "smooth" });
+
   console.log(section);
 });
