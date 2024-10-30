1522f4a938480e473d28b35be2fc8ef0053e3156
Tristverm <emmanuelkyalo72@gmail.com>
2024-10-30 21:35:38 +0300

    Added footer Container styling

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-window.addEventListener("load", () => {
-  const section = document.getElementsByClassName("cards")[0];
+// window.addEventListener("load", () => {
+//   const section = document.getElementsByClassName("cards")[0];
 
-  section?.scrollIntoView({ behavior: "smooth" });
+//   section?.scrollIntoView({ behavior: "smooth" });
 
-  console.log(section);
-});
+//   console.log(section);
+// });
 let date = new Date().getFullYear();
 const para = document.getElementsByClassName("year")[0];
 para.textContent = date.toString();
