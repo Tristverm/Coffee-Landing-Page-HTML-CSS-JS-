836a189dcfda7cb799c7f769590acad982676945
Tristverm <emmanuelkyalo72@gmail.com>
2024-11-02 11:20:06 +0300

    Added responsive styling for mobile navigation

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-window.addEventListener("load", () => {
-  const section = document.getElementsByClassName("cards")[0];
-  console.log(document.getElementsByClassName("cards"));
+// window.addEventListener("load", () => {
+//   const section = document.getElementsByClassName("cards")[0];
+//   console.log(document.getElementsByClassName("cards"));
 
-  section?.scrollIntoView({ behavior: "smooth" });
+//   section?.scrollIntoView({ behavior: "smooth" });
 
-  console.log(section);
-});
+//   console.log(section);
+// });
 let date = new Date().getFullYear();
 const para = document.getElementsByClassName("year")[0];
 para.textContent = date.toString();
 // console.log(para);
+const hamburgerButton = document.getElementById("hamburgerButton");
+const mobileViewNav = document.getElementById("mobileViewNav");
+hamburgerButton.addEventListener("click", () => {
+  mobileViewNav.classList.toggle("mobileViewNavDisplay");
+});
