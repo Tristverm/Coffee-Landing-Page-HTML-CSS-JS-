e9bf4f7d9fcda1864c2ebe258559ed353c9a672e
Tristverm <emmanuelkyalo72@gmail.com>
2024-11-02 08:32:27 +0300

    Added responsiveness

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// window.addEventListener("load", () => {
-//   const section = document.getElementsByClassName("cards")[0];
+window.addEventListener("load", () => {
+  const section = document.getElementsByClassName("cards")[0];
+  console.log(document.getElementsByClassName("cards"));
 
-//   section?.scrollIntoView({ behavior: "smooth" });
+  section?.scrollIntoView({ behavior: "smooth" });
 
-//   console.log(section);
-// });
+  console.log(section);
+});
 let date = new Date().getFullYear();
 const para = document.getElementsByClassName("year")[0];
 para.textContent = date.toString();
